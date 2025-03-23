eb1345b6bc09efa7c717ace2d8429846df25efd4
Ilias Sarris <Ilias.D.Sarris@outlook.com>
2025-03-23 11:08:37 +0200

    Initial site upload

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Hello World!",
+        "excerpt":"Welcome to my new blog! This is my first post, and I’m excited to start sharing my thoughts and experiences on a variety of topics, primarily focused on: Product Management: I’ll be sharing insights from my years in the field, discussing best practices, challenges, and the ever-evolving landscape of product...","categories": [],
+        "tags": [],
+        "url": "/2025/03/08/hello-world.html",
+        "teaser": null
+      },{
+        "title": "Prioritizing Features When Everything is 'High Priority'",
+        "excerpt":"Introduction: The Prioritization Paradox As product managers, we often face a seemingly endless stream of feature requests, bug fixes, and new ideas. A common problem is that many, if not most, of these items are labeled as “high priority” by stakeholders. This creates a prioritization paradox: if everything is a...","categories": [],
+        "tags": [],
+        "url": "/2025/03/11/prioritizing-features.html",
+        "teaser": null
+      },{
+        "title": "Prioritizing Features When Everything is \"High Priority\" (Casual)",
+        "excerpt":"Introduction: The Prioritization Paradox As product managers, we often face a large number of feature requests, bug fixes, and new ideas. A common challenge is that many of these items are labeled as “high priority” by stakeholders. This creates a prioritization paradox: if everything is a high priority, then nothing...","categories": [],
+        "tags": [],
+        "url": "/2025/03/12/prioritizing-features-casual.html",
+        "teaser": null
+      }]
